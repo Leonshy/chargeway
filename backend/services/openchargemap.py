06108d81e9dbf9ff2661d5778f5773eca5cc0d0a
da21nny <edgarv21.ev@gmail.com>
2026-02-03 11:32:18 -0300

    agregamos el marcador

--- backend/services/openchargemap.py
+++ backend/services/openchargemap.py
@@ -16,9 +16,52 @@ def obtener_estaciones():
 
 
 def generar_mapa():
-    estaciones = obtener_estaciones()
-    mapa = folium.Map(location=[-23.442503, -58.443832], zoom_start=6)
+    # Coordenadas de Paseo La Galería
+    lat, lon = -25.2844487, -57.5631777
+    mapa = folium.Map(location=[lat, lon], zoom_start=15)
 
+    map_id = mapa.get_name()
+
+    script_marcador = f"""
+    <script>
+        var marcador_usuario = null;
+        
+        function inicializarClic() {{
+            // Verificamos si el objeto mapa ya existe en el navegador
+            if (typeof {map_id} !== 'undefined') {{
+                
+                // Escuchar el evento click
+                {map_id}.on('click', function(e) {{
+                    // Si ya hay un marcador, lo borramos para que solo quede uno
+                    if (marcador_usuario) {{
+                        {map_id}.removeLayer(marcador_usuario);
+                    }}
+                    
+                    // Crear el nuevo marcador donde se hizo clic
+                    marcador_usuario = L.marker(e.latlng).addTo({map_id});
+                    
+                    // Mostrar popup con coordenadas
+                    marcador_usuario.bindPopup(
+                        "<b>Ubicación seleccionada</b><br>" + 
+                        e.latlng.lat.toFixed(5) + ", " + e.latlng.lng.toFixed(5)
+                    ).openPopup();
+                }});
+
+            }} else {{
+                // Si el mapa aún no carga, reintentamos en 100ms
+                setTimeout(inicializarClic, 100);
+            }}
+        }}
+
+        // Iniciamos el script
+        setTimeout(inicializarClic, 500);
+    </script>
+    """
+    
+    # Inyectamos el script en el HTML del mapa
+    mapa.get_root().html.add_child(folium.Element(script_marcador))
+
+    estaciones = obtener_estaciones()
     for e in estaciones:
         info = e.get("AddressInfo")
         if info:
